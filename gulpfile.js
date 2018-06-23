5d7d622b392114e70a9e07e92141b27bcbbe7d4b
Abdurrahman Hafez <abdurrahman.hafez@gmail.com>
2018-06-23 19:34:22 +0200

    Design a grid for the restaurant page

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
-var concat = require('gulp-concat');
+var gulp          = require('gulp');
+var sass          = require('gulp-sass');
+var autoprefixer  = require('gulp-autoprefixer');
+var browserSync   = require('browser-sync').create();
+var concat        = require('gulp-concat');
 
-gulp.task('default', ['copy-html', 'copy-images', 'styles', 'scripts'], function () {
+gulp.task('default', ['copy-html', 'copy-images', 'styles', 'scripts', 'copy-restaurant'], function () {
   gulp.watch('src/sass/**/*.scss', ['styles']);
   gulp.watch('src/index.html', ['copy-html']);
-  gulp.watch('src/restaurant.html', ['copy-html']);
+  gulp.watch('src/restaurant.html', ['copy-restaurant']);
   gulp.watch('src/js/**/*.js', ['scripts']);
   gulp.watch('./dist/index.html').on('change', browserSync.reload);
   gulp.watch('./dist/restaurant.html').on('change', browserSync.reload);
@@ -23,7 +23,8 @@ gulp.task('dist', [
   'copy-html',
   'copy-images',
   'styles',
-  'scripts-dist'
+  'scripts-dist',
+  'copy-restaurant'
 ]);
 
 gulp.task('scripts', function () {
@@ -38,6 +39,11 @@ gulp.task('scripts-dist', function () {
 
 gulp.task('copy-html', function () {
   gulp.src('src/index.html')
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy-restaurant', function () {
+  gulp.src('src/restaurant.html')
     .pipe(gulp.dest('./dist'));
 });
 
@@ -56,5 +62,3 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.stream());
 });
-
-// outputStyle: 'compressed'
